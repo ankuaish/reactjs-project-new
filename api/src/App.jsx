571bd98c5dd61bd2d202b2929e61cfcc6c52d682
ankuaish <anku.kesharwani17@gmail.com>
2025-08-23 13:42:52 +0530

    api

--- api/src/App.jsx
+++ api/src/App.jsx
@@ -1,7 +1,8 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [users, setUser] = useState([]);
   useEffect(() => {
     getUserData();
   }, []);
@@ -9,11 +10,23 @@ function App() {
     const url = "https://dummyjson.com/users";
     let response = await fetch(url);
     response = await response.json();
-    console.log(response);
+    setUser(response.users);
   };
   return (
     <>
-      <h1>Fetch/Get Data from API</h1>
+      <h2>Fetch/Get Data from API</h2>
+      <ul className="user-list">
+        <li>First Name</li>
+        <li>Last Name </li>
+        <li>Age</li>
+      </ul>
+      {users.map((item, index) => (
+        <ul key={index} className="user-list">
+          <li>{item.firstName}</li>
+          <li>{item.lastName}</li>
+          <li>{item.age}</li>
+        </ul>
+      ))}
     </>
   );
 }
