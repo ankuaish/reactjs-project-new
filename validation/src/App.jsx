3a904caf29cf294ad7f2d1828a324845ed5ae949
Aishwarya <anku.kesharwani17@gmail.com>
2025-08-27 20:02:08 +0530

    usered

--- validation/src/App.jsx
+++ validation/src/App.jsx
@@ -1,51 +1,56 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-
-import './App.css'
-import UseAct from './UseAct';
+import "./App.css";
+import UseAct from "./UseAct";
 
 function App() {
-
   const [nameErr, setNameErr] = useState();
 
- 
-  const [passErr, setPassErr] = useState();  
+  const [passErr, setPassErr] = useState();
 
-  const handleName =(event) =>{
-if(event.target.value.length>5){
-setNameErr("Please enter valid user name only 5 characters allowed")
-}else{
-  setNameErr("")
-}
-  }
+  const handleName = (event) => {
+    if (event.target.value.length > 5) {
+      setNameErr("Please enter valid user name only 5 characters allowed");
+    } else {
+      setNameErr("");
+    }
+  };
 
-    const handlePass =(event) =>{
-      let regex = /^[A-Z0-9]+$/i;
-if(regex.test(event.target.value)){
-    setPassErr("")
-
-}else{
-  setPassErr("Please enter numbers and alphabet only")
-}
-  }
+  const handlePass = (event) => {
+    let regex = /^[A-Z0-9]+$/i;
+    if (regex.test(event.target.value)) {
+      setPassErr("");
+    } else {
+      setPassErr("Please enter numbers and alphabet only");
+    }
+  };
   return (
     <>
-    <div>
-      <input type="text" className={nameErr?"error": ""} onChange={handleName} placeholder='enter your name' />
-      <span style={{color:"red"}}>{nameErr && nameErr}</span>
-      <br />
-      <br />
-       <input type="text"className={passErr?"error": ""} onChange={handlePass} placeholder='enter your password' />
-         <span style={{color:"red"}}>{passErr && passErr}</span>
-         <br />
-      <br />
-       <button disabled={nameErr || passErr}>Login</button>
-    </div>
-      
+      <div>
+        <input
+          type="text"
+          className={nameErr ? "error" : ""}
+          onChange={handleName}
+          placeholder="enter your name"
+        />
+        <span style={{ color: "red" }}>{nameErr && nameErr}</span>
+        <br />
+        <br />
+        <input
+          type="text"
+          className={passErr ? "error" : ""}
+          onChange={handlePass}
+          placeholder="enter your password"
+        />
+        <span style={{ color: "red" }}>{passErr && passErr}</span>
+        <br />
+        <br />
+        <button disabled={nameErr || passErr}>Login</button>
+      </div>
 
-      <UseAct/>
+      <UseAct />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ validation/src/UseRed.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const UseRed = () => {
+  return (
+    <div>
+      <h1>UseReducer for State Management </h1>
+      <input type="text" placeholder="enter your name" />
+      <br />
+      <br />
+      <input type="text" placeholder="enter your password" />
+      <br />
+      <br />
+      <input type="text" placeholder="enter your email" />
+      <br />
+      <br />
+      <input type="text" placeholder="enter your address" />
+      <br />
+      <br />
+      <input type="text" placeholder="enter your city" />
+      <br />
+      <br />
+      <button>submit</button>
+    </div>
+  );
+};
+
+export default UseRed;
