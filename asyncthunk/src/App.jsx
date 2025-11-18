604a311c4dd6d958bbceaea8f9fc68bf22653507
Aishwarya Kesharwani <anku.kesharwani17@gmail.com>
2025-11-18 20:21:32 +0530

    hahah

--- asyncthunk/src/App.jsx
+++ asyncthunk/src/App.jsx
@@ -1,13 +1,13 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchUsers } from './userSlice';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchUsers } from "./userSlice";
 
 function Users() {
   const dispatch = useDispatch();
   const { users, loading, error } = useSelector((state) => state.users);
 
   useEffect(() => {
-    dispatch(fetchUsers());  // call the thunk
+    dispatch(fetchUsers()); // call the thunk
   }, []);
 
   if (loading) return <p>Loading...</p>;
